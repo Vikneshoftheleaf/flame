16946c8c2545cbcc05ff21a8abe73eadc3fc514f
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2023-10-02 14:27:54 +0530

    theata

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const port = 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname +"/index.html")
 })
@@ -17,6 +23,35 @@ app.get('/coming',(req,res)=>{
 app.get('/flame',(req,res)=>{
     res.sendFile(__dirname +"/flame.html")
 })
+
+
+app.post('/submit', (req, res) => {
+    const formData = {
+        person1: req.body.name1,
+        person2: req.body.name2,
+        mode: req.body.mode
+    };
+
+    // Load existing data from the JSON file
+    let data = [];
+    try {
+        const existingData = fs.readFileSync('data.json', 'utf-8');
+        data = JSON.parse(existingData);
+    } catch (error) {
+        console.error('Error reading data from JSON file:', error);
+    }
+
+    // Add the new form data
+    data.push(formData);
+
+    // Save the updated data to the JSON file
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (error) => {
+        if (error) {
+            console.error('Error writing data to JSON file:', error);
+            res.status(500).send('Error saving data');
+        } 
+    });
+});
 
 app.listen(port,()=>{
     console.log(`Listeneing at ${port}`)
